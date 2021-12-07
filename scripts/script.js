c1ac334fa562606c6c19b071fcac13bc51510a1c
christiaandirven <85060636+christiaandirven@users.noreply.github.com>
2021-12-07 10:18:14 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,14 @@
 // JavaScript Document
 
-var navMove
+var deButton = document.querySelector("nav:nth-of-type(1) button");
+var navMove = document.querySelector("nav:nth-of-type(2)");
+var tweedeButton = document.querySelector("nav:nth-of-type(2) button");
 
-var navMove =
-    document.querySelector("nav:nth-of-type(2)");
-navMove.addEventListener("click", leftToRight);
+
+deButton.addEventListener("click", leftToRight);
+tweedeButton.addEventListener("click", leftToRight);
+
 
 function leftToRight() {
-
     navMove.classList.toggle("toonMenu");
 }
